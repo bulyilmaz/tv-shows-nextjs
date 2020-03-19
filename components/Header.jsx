aa560e819fa4f34a78f748acb6b729dd8cbac29d
Bülent Yılmaz <bulyilmaz@gmail.com>
2020-03-19 14:51:44 +0300

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,20 +1,18 @@
 import { useState } from "react";
-import Router, { useRouter } from "next/router";
+import { useRouter } from "next/router";
 
 const Header = () => {
     const router = useRouter();
-    const selectedCountryInitialValue = router.query && router.query.country ? router.query.country : "";
+    const selectedCountryInitialValue = router.query && router.query.country ? router.query.country : "us";
     const [selectedCountry, setSelectedCountry] = useState(selectedCountryInitialValue);
     const handleChange = event => {
         setSelectedCountry(event.target.value);
-        //router.push("/[country]", `/${event.target.value}`);
-        Router.push(`/${event.target.value}`);
+        router.push("/[country]", `/${event.target.value}`);
     };
 
     return (
         <div>
             <select value={selectedCountry} onChange={handleChange}>
-                <option value="">Country</option>
                 <option value="us">USA</option>
                 <option value="tr">Turkey</option>
             </select>
